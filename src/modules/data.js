71de446b20c867260cf0af2e26f56650e95a12de
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-15 01:16:23 +0200

    Update index.js file >> setup the local storage

--- src/modules/data.js
+++ src/modules/data.js
@@ -7,11 +7,9 @@ class Task {
 }
 
 const addTaskForm = document.querySelector('#add-task');
-const data = JSON.parse(localStorage.getItem('Task-list')) || [];
 addTaskForm.addEventListener('submit', () => {
   const inputTaskValue = document.querySelector('#add-task-input').value;
   if (inputTaskValue === '') return;
   const newTask = new Task(inputTaskValue);
   data.push(newTask);
-  localStorage.setItem('Task-list', JSON.stringify(data));
 });
