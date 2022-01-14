e02dec518ceeb2ab6ca3a7c7de6e2999b27893d8
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-15 01:06:47 +0200

    Update the index.js file and changes some functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import './style.css';
 import Data from './modules/data.js';
-import { addNewTask } from './modules/updateTasks.js';
 
-const tasksList = document.querySelector('.list');
-const addTaskInput = document.querySelector('.add-task-input');
-const addIcon = document.querySelector('.plus-circle-icon');
+function getInputValue(task) {
+  return task.description;
+}
 
-addIcon.addEventListener('click', () => {
-  if (addTaskInput.value === '') {
-    alert('Field required');
-  } else {
-    addNewTask(addTaskInput.value, tasksList, Data);
-    Data.displayTask();
-    document.querySelector('.add-task-input').value = '';
-  }
+getTask().forEach((task) => {
+  taskList.innerHTML += `<li class="container task flex-center" draggable="true">
+  <span class="left flex-center">
+    <input id=${task.index} type="checkbox" ${
+  task.completed ? 'checked' : ''
+}  class="checkbox" />
+    <form class="edit-form" action="/">
+    <input data-index-number=${task.index} value='${getInputValue(
+  task,
+)}' class="${task.completed ? 'edit-task disabled' : 'edit-task'}" ${
+  task.completed ? 'disabled' : ''
+} ></form>
+  </span>
+  <span class="right">
+    <i class="fas fa-ellipsis-v"></i>
+  </span>
+</li>`;
 });
-
-Data.displayTask();
