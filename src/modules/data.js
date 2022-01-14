b1edc15af698376df4203e9ffc9bcdd6dc848a3e
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-14 03:42:50 +0200

    Implemet the localsorage of data

---
+++ src/modules/data.js
@@ -0,0 +1,55 @@
+// const tasks = [
+//   {
+//     index: 0,
+//     description: 'Attend morning  session',
+//     completed: true,
+//   },
+//   {
+//     index: 1,
+//     description: 'Submit the project',
+//     completed: true,
+//   },
+//   {
+//     index: 2,
+//     description: 'Prepare my interview',
+//     completed: true,
+//   },
+//   {
+//     index: 3,
+//     description: 'Attend the standup call',
+//     completed: false,
+//   },
+// ];
+
+export default class Data {
+  static getAllData() {
+    return JSON.parse(localStorage.getItem('Tasks'));
+  }
+
+  static storeData(tasks) {
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
+  }
+
+  static getData(index) {
+    const allData = Data.getAllData();
+    return allData.filter((adata) => adata.index === index)[0];
+  }
+
+  static displayTask(listContainer) {
+    const allTasks = Data.getAllData();
+
+    allTasks.forEach((atask) => {
+      const listContainer = document.querySelector('.list');
+      listContainer.classList.add('task-list');
+      listContainer.innerHTML += `<li class="task-item">
+                                      <form id="form2" class="form">
+                                        <input class="check-input" value="checked" type="checkbox" id="check">
+                                        <input class="task-input2" value="${atask.description}" type="text" id="task">
+                                      </form >
+                                      <span class="ellipsis-icon clickable">&#8942;</i></span>
+                                 </li>`;
+    });
+    return listContainer;
+  }
+}
+
