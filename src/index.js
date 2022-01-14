509d9ed20c94c692336d1e6230a8f1500a007c92
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-15 01:29:50 +0200

    Implement the edit form functionnality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/prefer-default-export */
 import './style.css';
 import { getTask } from './modules/data.js';
+
+export const taskList = document.querySelector('.task-list-container');
 
 function getInputValue(task) {
   return task.description;
@@ -22,4 +25,22 @@ getTask().forEach((task) => {
     <i class="fas fa-ellipsis-v"></i>
   </span>
 </li>`;
+});
+
+export const task = document.querySelectorAll('.task');
+export const editTask = document.querySelectorAll('.edit-task');
+const editForm = document.querySelectorAll('.edit-form');
+
+editForm.forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    editTask.forEach((taskList) => {
+      getTask().forEach((task) => {
+        if (taskList.dataset.indexNumber === task.index) {
+          task.description = taskList.value;
+          localStorage.setItem('Task-list', JSON.stringify(getTask()));
+        }
+      });
+    });
+  });
 });
