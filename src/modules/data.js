b8274f762600a2d93e2d698078f37e725085c3a0
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-15 02:29:05 +0200

    move content to another file

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,37 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-restricted-globals */
-class Task {
-  constructor(description) {
-    this.description = description;
-    this.index = new Date();
-    this.completed = false;
-  }
-}
 
-const addTaskForm = document.querySelector('#add-task');
-const data = JSON.parse(localStorage.getItem('Task-list')) || [];
-addTaskForm.addEventListener('submit', () => {
-  const inputTaskValue = document.querySelector('#add-task-input').value;
-  if (inputTaskValue === '') return;
-  const newTask = new Task(inputTaskValue);
-  data.push(newTask);
-  localStorage.setItem('Task-list', JSON.stringify(data));
-});
+import { addTask } from './controllTools.js';
 
-const clearTask = document.querySelector('.clear-task');
-
-clearTask.addEventListener('click', () => {
-  if (data.length > 0) {
-    localStorage.clear('Task-list');
-    location.reload();
-  }
-});
-
-const reload = document.querySelector('.reload');
-reload.addEventListener('click', () => {
-  location.reload();
-});
-
+const data = addTask();
 export function getTask() {
   return data.map((task) => task);
 }
