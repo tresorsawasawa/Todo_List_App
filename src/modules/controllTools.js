4d9773c871de110fd8e9c7bd6048b6c60f662316
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-15 02:24:36 +0200

    Add the function to add a task

--- src/modules/controllTools.js
+++ src/modules/controllTools.js
@@ -13,6 +13,26 @@ export function deleteTask() {
   });
 }
 
+export function addTask() {
+  class Task {
+    constructor(description) {
+      this.description = description;
+      this.index = new Date();
+      this.completed = false;
+    }
+  }
+  const addTaskForm = document.querySelector('#add-task');
+  const data = JSON.parse(localStorage.getItem('Task-list')) || [];
+  addTaskForm.addEventListener('submit', () => {
+    const inputTaskValue = document.querySelector('#add-task-input').value;
+    if (inputTaskValue === '') return;
+    const newTask = new Task(inputTaskValue);
+    data.push(newTask);
+    localStorage.setItem('Task-list', JSON.stringify(data));
+  });
+  return data;
+}
+
 export function deleteOne(deleteIcon, taskId) {
   deleteIcon.addEventListener('click', () => {
     const filteredTask = getTask().filter((task) => task.index !== taskId);
